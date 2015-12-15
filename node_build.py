c84ccdd7b8a8c18558c64ed2bc532b8dfe1b2345
Peter Panaguiton <paspanag@gmail.com>
2015-12-15 18:30:16 -0500

    new changes

--- node_build.py
+++ node_build.py
@@ -4,6 +4,8 @@ import os
 
 PROJECT_PATH="project"
 RR_NODEJS="rumprun-packages/nodejs"
+BUILDS_DIR="builds"
+NODE_BIN="node.bin"
 
 #rm -rf $FLNAME
 #git clone $REPO $FLNAME
@@ -43,17 +45,24 @@ def init_project(project_dir):
         with cd(project_dir):
                 subprocess.call(["npm", "install"])
 
-def package_project(project_dir, app_name):
+def package_project(project_dir, app_name, build_dir):
         subprocess.call(["genisoimage", "-l" ,"-r" ,"-o" ,app_name , project_dir])
-        subprocess.call(["mkdir", "-p", "app_builds"])
-        subprocess.call(["mv", app_name, "app_builds/"])
+        subprocess.call(["mkdir", "-p", build_dir])
+        subprocess.call(["mv", app_name, build_dir])
 
 def hash_project(project_dir):
         with cd(project_dir):
                 return subprocess.check_output(["nix-hash", "."])
 
+def package_with_binary(build_name, builds_dir, app_name, node_bin):
+	with cd(builds_dir):
+		subprocess.call(["zip", build_name, app_name, node_bin])
+	
+
 if __name__ == "__main__":
         if not check_project(PROJECT_PATH):
                 clone_project(sys.argv[1], PROJECT_PATH)
         init_project(PROJECT_PATH)
-        package_project(PROJECT_PATH, "app.iso")
+	app_name = "app.iso"
+        package_project(PROJECT_PATH, app_name, BUILDS_DIR)
+	package_with_binary("appAndNode", BUILDS_DIR, app_name, NODE_BIN)
